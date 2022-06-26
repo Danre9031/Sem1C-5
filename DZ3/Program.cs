fd36c4ea8d888b73a817f1f4884ee46ecbe24ee7
Mihail <Dante9041@mail.ru>
2022-06-26 12:32:15 +0300

    add DZ3

---
+++ DZ3/Program.cs
@@ -0,0 +1,55 @@
+﻿int size = Input("Введи размер: ");
+int[] array = new int[size];
+
+FillArrayRandomNumbers(array);
+PrintArray(array);
+Console.WriteLine(SumMaxAMinNumbers(array));
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int size = arr.Length;
+    Console.Write($"[");
+    for (int i = 0; i < size - 1; i++)
+    {
+        Console.Write($"{arr[i]},");
+    }
+    Console.Write($"{arr[size - 1]}]");
+    Console.WriteLine();
+}
+
+int SumMaxAMinNumbers(int[] array)
+{
+    int result = 0;
+    int temp = 1;
+    int max = int.MinValue;
+    int min = int.MaxValue;
+    for (int i = 0; i < array.Length; i++)
+    {
+        temp = array[i];
+        if (temp > max)
+        {
+            max = temp;
+        }
+        else if (temp < min)
+        {
+            min = temp;
+        }
+
+    }
+    result = max - min;
+    return result;
+
+}
